640bf87cda5546e47e66c3de31ebc66f46d90d00
Austin McLain <93621884+austinmclain@users.noreply.github.com>
2024-02-25 10:28:09 -0600

    Fix OpenJDK installation

--- aws/amazon-linux-jenkins.sh
+++ aws/amazon-linux-jenkins.sh
@@ -4,7 +4,7 @@ wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum upgrade -y
-yum install fontconfig java-17-openjdk -y
+yum install fontconfig java-17-amazon-corretto -y
 yum install jenkins -y
 systemctl daemon-reload
 systemctl start jenkins
